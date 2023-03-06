ff75054cecd3118b8644995526e20115d3832829
Guimill <86968380+Guimill@users.noreply.github.com>
2023-03-06 19:43:16 +0100

    ajout img

--- cartoon/src/App.js
+++ cartoon/src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import test from './assets/img/homer.jpg'
+import linkedin from './assets/img/linkedin.svg'
+import github from './assets/img/github.svg'
+import mail from './assets/img/mail.svg'
 
 function App() {
   return (
@@ -6,18 +10,18 @@ function App() {
           <header class="banner" role="banner">
         <div class="banner__containerTop">
             <h1>CARTOON POLITICS</h1>
-            <p> permet de positionner correctement le banner avec space-around </p>
-            <p> permet de positionner correctement le banner avec space-around </p>
-            <p> permet de positionner correctement le banner avec space-around </p>
+            <p> {/*permet de positionner correctement le banner avec space-around*/} </p>
+            <p> {/*permet de positionner correctement le banner avec space-around*/} </p>
+            <p> {/*permet de positionner correctement le banner avec space-around*/} </p>
             <div>
-                <a href="index.html"><button class="banner__containerTop__buttonLangue"><h3>FR</h3></button></a>
+                <a href="./public/index.html"><button class="banner__containerTop__buttonLangue"><h3>FR</h3></button></a>
                 <a href="https://en.duolingo.com/course/fr/en/Learn-French"><button class="banner__containerTop__buttonLangue"><h3>EN</h3></button></a>
             </div>
         </div>
         <div class="banner__containerBottom">
-            <a class="banner__containerBottom__a" href="guillaume.milleret@gmail.com"><img class="banner__containerBottom__a__img" src="./public/img/mail.svg" alt="logo d'une enveloppe contenant mon mail"></img></a>
-            <a class="banner__containerBottom__a" href="https://github.com/Guimill"><img class="banner__containerBottom__a__img" src="./public/img/github.svg" alt="logo de github redirigeant vers mon github"></img></a>
-            <a class="banner__containerBottom__a" href="https://www.linkedin.com/in/guillaume-milleret/"><img class="banner__containerBottom__a__img" src="./public/img/linkedin.svg" alt="logo de linkedin redirigeant vers mon linkedin"></img></a>
+            <a class="banner__containerBottom__a" href="guillaume.milleret@gmail.com"><img class="banner__containerBottom__a__img" src={mail} alt="logo d'une enveloppe contenant mon mail"></img></a>
+            <a class="banner__containerBottom__a" href="https://github.com/Guimill"><img class="banner__containerBottom__a__img" src={github} alt="logo de github redirigeant vers mon github"></img></a>
+            <a class="banner__containerBottom__a" href="https://www.linkedin.com/in/guillaume-milleret/"><img class="banner__containerBottom__a__img" src={linkedin} alt="logo de linkedin redirigeant vers mon linkedin"></img></a>
         </div>
     </header>
 
@@ -25,7 +29,7 @@ function App() {
         <section>
             <div class="personnage" >
                 <h2>  Simpson Homer  </h2>
-                <img class="personnage__img" src="./public/img/homer.jpg" alt="test"></img>
+                <img class="personnage__img" src={test} alt="test"></img>
             </div>
         </section>
 
