cbc3400cdff745695c07f83d679eb73d8ec006cf
Guimill <86968380+Guimill@users.noreply.github.com>
2023-03-08 17:21:52 +0100

    some change in imggallery

--- src/components/ImageGallery/index.jsx
+++ src/components/ImageGallery/index.jsx
@@ -1,22 +1,24 @@
 import { personnagesList } from "../../datas/personnagesList";
 import Card from "../card";
+import styled from "styled-components";
+
+const ImgGallery = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  
+`
 
 function ImageGallery() {
-
-    return (
-            <ul className='ImageGallery'>
-				{personnagesList.map(({id, src, name}) => (
-						<div key={id}>
-							<Card
-								name={name}
-                                src={src}
-							/>
-						</div>
-					)
-				)}
-			</ul>
-    )
-
+  return (
+    <ImgGallery className="ImageGallery">
+      {personnagesList.map(({ id, src, name }) => (
+        <div key={id}>
+          <Card name={name} src={src} />
+        </div>
+      ))}
+     </ImgGallery>
+  );
 }
 
-export default ImageGallery
+export default ImageGallery;
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,12 +1,10 @@
 function Card({name, src}) {
 
     return (
-        <section role="section de présentation du personnage">
             <div class="personnage" >
                 <h2>{name}</h2>
                 <img class="personnage__img" src={src} alt={name} />
             </div>
-        </section>
     );
   }
 
