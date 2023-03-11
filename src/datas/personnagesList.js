08f175e709f5b586629bbb15fa316cc337363de2
Guimill <86968380+Guimill@users.noreply.github.com>
2023-03-12 00:33:21 +0100

    rajout de personnages

--- src/datas/personnagesList.js
+++ src/datas/personnagesList.js
@@ -7,6 +7,31 @@ import Ralph from '../assets/images/ralph.png'
 import Moe from '../assets/images/moe.png'
 import Skinner from '../assets/images/skinner.png'
 import Couch from '../assets/images/couch.png'
+import Brockman from '../assets/images/brockman.png'
+import Ned from '../assets/images/ned.png'
+import Manjula from '../assets/images/manjula.png'
+import Patty from '../assets/images/patty.png'
+import Selma from '../assets/images/selma.png'
+import Tetina from '../assets/images/tetina.png'
+import Edna from '../assets/images/edna.png'
+import Apu from '../assets/images/apu.png'
+import Abraham from '../assets/images/abraham.png'
+import Janine from '../assets/images/janine.png'
+import Sarah from '../assets/images/sarah.png'
+import Scratchy from '../assets/images/scratchy.png'
+import Itchy from '../assets/images/itchy.png'
+import Frink from '../assets/images/frink.png'
+import Sherri from '../assets/images/sherri.png'
+import Luann from '../assets/images/luann.png'
+import Spider from '../assets/images/spider.png'
+import Bob from '../assets/images/bob.png'
+import Krusty from '../assets/images/krusty.png'
+import Duffman from '../assets/images/duffman.png'
+import Smithers from '../assets/images/smithers.png'
+
+
+
+
 
 
 
@@ -55,5 +80,110 @@ export const personnagesList = [
         id:9,
         name: 'Couch',
         src: Couch
+    },
+    {
+        id:10,
+        name: 'Abraham',
+        src: Abraham
+    },
+    {
+        id:11,
+        name: 'Duffman',
+        src: Duffman
+    },
+    {
+        id:12,
+        name: 'Apu',
+        src: Apu
+    },
+    {
+        id:13,
+        name: 'Manjula',
+        src: Manjula
+    },
+    {
+        id:14,
+        name: 'Itchy',
+        src: Itchy
+    },
+    {
+        id:15,
+        name: 'Scratchy',
+        src: Scratchy
+    },
+    {
+        id:16,
+        name: 'Edna',
+        src: Edna
+    },
+    {
+        id:17,
+        name: 'Tetina',
+        src: Tetina
+    },
+    {
+        id:18,
+        name: 'Selma',
+        src: Selma
+    },
+    {
+        id:19,
+        name: 'Patty',
+        src: Patty
+    },
+    {
+        id:20,
+        name: 'Jumelles',
+        src: Sherri
+    },
+    {
+        id:21,
+        name: 'Janine',
+        src: Janine
+    },
+    {
+        id:22,
+        name: 'Sarah',
+        src: Sarah
+    },
+    {
+        id:23,
+        name: 'Luann',
+        src: Luann
+    },
+    {
+        id:24,
+        name: 'Ned',
+        src: Ned
+    },
+    {
+        id:25,
+        name: 'Smithers',
+        src: Smithers
+    },
+    {
+        id:26,
+        name: 'SpiderCochon',
+        src: Spider
+    },
+    {
+        id:27,
+        name: 'Frink',
+        src: Frink
+    },
+    {
+        id:28,
+        name: 'Brockman',
+        src: Brockman
+    },
+    {
+        id:29,
+        name: 'Krusty',
+        src: Krusty
+    },
+    {
+        id:30,
+        name: 'Bob',
+        src: Bob
     }
 ]
