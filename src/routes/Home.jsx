7fc5c3235ec0d96cad327eb6d5cc772ed58cc869
Guimill <86968380+Guimill@users.noreply.github.com>
2023-03-13 22:20:31 +0100

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,6 @@
 import Header from '../routes/Header/Header'
 import Footer from '../routes/Footer/Footer'
-import ImageGallery from '../components/imageGallery/index'
+import ImageGallery from '../components/imageGallery/index.jsx'
 
 function Home() {
   return (
